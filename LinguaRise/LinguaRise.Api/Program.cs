8c00c64355bdd2f97838892e8abcf6b3dd4a67ce
Piotr Równicki <piotr.rownicki@gearcode.eu>
2025-03-24 14:15:03 +0100

    Refactoring of the backend and frontend structure

--- LinguaRise/LinguaRise.Api/Program.cs
+++ LinguaRise/LinguaRise.Api/Program.cs
@@ -18,9 +18,6 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-app.UseDefaultFiles();
-app.UseStaticFiles();
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -33,7 +30,5 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
-
-app.MapFallbackToFile("/index.html");
 
 app.Run();
