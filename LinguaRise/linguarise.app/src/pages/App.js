1ca0792767edeaf0bcf83a94c40486b9d997c8da
fiZZowsky <piotrekr53@interia.pl>
2025-03-25 07:33:06 +0100

    Some fixes on frontend site

--- LinguaRise/linguarise.app/src/pages/App.js
+++ LinguaRise/linguarise.app/src/pages/App.js
@@ -1,23 +1,10 @@
-import logo from '../assets/images/logo.svg';
+import React from 'react';
 import '../assets/styles/App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>LinguaRise</h1>
     </div>
   );
 }
